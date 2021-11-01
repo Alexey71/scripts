6651ed763e0bf9cd2561b7a5632e9adbfd776591
Alexey <agarkov.alexey.viktorovich@gmail.com>
2021-11-01 19:59:11 +0300

    Update RuTor.org_Seed_Graph_Mod_&_Search_Filter.js

--- RuTor.org_Seed_Graph_Mod_&_Search_Filter.js
+++ RuTor.org_Seed_Graph_Mod_&_Search_Filter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       RuTor.org Seed Graph Mod & Search Filter22222
 // @namespace  https://github.com/Alexey71/script-adguard/blob/master/RuTor.org_Seed_Graph_Mod_&_Search_Filter.js
-// @version    0.5.5
+// @version    0.5.6
 // @description  Помогает визуально увидить популярность той или иной раздачи. Быстрый поиск на текущей странице
 // @match       http://rutorc6mqdinc4cz.onion/
 // @match       http://tor-ru.net/*
