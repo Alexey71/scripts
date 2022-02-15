bb8464d7bcb9a51b370600a0551dd37e1ca6f7b9
Alexey <agarkov.alexey.viktorovich@gmail.com>
2022-02-15 15:07:11 +0300

    Update Kinozal.tv_Magnet_Links_Mod.js

--- Kinozal.tv_Magnet_Links_Mod.js
+++ Kinozal.tv_Magnet_Links_Mod.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name         kinozal.tv magnet links MOD
 // @namespace    https://raw.githubusercontent.com/Alexey71/scripts/master/Kinozal.tv_Magnet_Links_Mod.js
-// @version      0.5
+// @version      0.6
 // @description  add magnet links near to torrent links for kinozal.tv pages
 // @author       vike
 // @include      /^https?:\/\/kinozal.(tv|guru|me)\/details.php\?id=\d+$/
 // @grant        none
+/* eslint-env jquery */
 // ==/UserScript==
 
 $(function(){
     var imgURL = 'https://raw.githubusercontent.com/Alexey71/scripts/master/Kinozal.tv_Magnet_Links_Mod.jpg';
-    $("td[class='nw']").after('<td style="width: 20px"> <a id="magnetLink" title="Скачать раздачу используя magnet ссылку" href="#"> <img src="' + imgURL + '"/> </a> </td>');
+    $("a[href*='dl.kinozal.tv/download.php?id=']").before('<a id="magnetLink" title="Скачать раздачу используя magnet-ссылку" href="#"> <img src="' + imgURL + '" alt="Скачать раздачу используя magnet-ссылку" height="25"/> </a>');
 
     $("#magnetLink").click(function(){
         var result = new RegExp('id=(\\d+)').exec(window.location.href);
